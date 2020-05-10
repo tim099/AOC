9272782ce06d19c03580789bed0cfe5d82d40432
tim1125 <tim11251994@gmail.com>
2020-05-10 16:01:36 +0800

    Add AOC_GameConfig.cs Add AOC_GameManager.cs Add AOC_Game.unity Add AOC_Start.unity Add AOC_MainMenu.unity

---
+++ AOC_Scripts/AOC_GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UCL;
+namespace AOC {
+    public class AOC_GameManager : UCL.Core.Game.UCL_GameManager {
+        protected override void Init() {
+            if(f_Inited) return;
+
+
+
+            base.Init();
+            if(!f_Inited) {//base.Init() fail!!
+                return;
+            }
+            Debug.LogWarning("AOC_GameManager Init!!");
+            Debug.LogWarning("Test4:" + m_GameConfig.GetString("Test4"));
+            m_GameConfig.SetValue("Test4",System.DateTime.Now.ToString("HHmmss"));
+        }
+    }
+}
