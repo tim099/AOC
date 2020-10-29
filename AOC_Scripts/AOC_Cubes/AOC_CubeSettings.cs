0242c23722e170d0ce221bc826409e3db2daa687
tim1125 <tim11251994@gmail.com>
2020-10-29 20:52:22 +0800

    Update AOC

---
+++ AOC_Scripts/AOC_Cubes/AOC_CubeSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AOC
+{
+    [CreateAssetMenu(fileName = "CubeSettings", menuName = "AOC/CubeSettings")]
+    public class AOC_CubeSettings : ScriptableObject
+    {
+        public Texture2D m_Texture;
+        public Texture2D m_NormalTexture;
+    }
+
+}
+
